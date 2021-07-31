31416ee16db976a4e445c13fc68f2b6c96e710c1
Enemy <63464137+Brijeshkrishna@users.noreply.github.com>
2021-07-31 01:58:16 -0800

    Add files via upload

---
+++ src/assets/script.js
@@ -0,0 +1,13 @@
+function move() {
+	var apperer = document.getElementById('move');
+	var text = document.getElementById('texter');
+	apperer.innerHTML = "I<b> Can't</b> see U 🙈 "
+	text.innerHTML = " "
+}
+
+function remove() {
+	var apperer = document.getElementById('move');
+	var text = document.getElementById('texter');
+	apperer.innerHTML = ""
+	text.innerHTML = '_ = { " Hi " , " I am Brijesh " } ;'
+}
